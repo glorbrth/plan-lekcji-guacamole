c58c3c89bf9814669e455b21c70c10c69bb52022
glorbrth <jan.gizycki.zse@gmail.com>
2026-03-19 10:17:05 +0100

    Update kreator_planow.js

--- plan_lekcji/kreator_planow.js
+++ plan_lekcji/kreator_planow.js
@@ -21,6 +21,15 @@ function add_minutes(time, minutes) {
 	return [time[0] + div, rem];
 }
 
+/// integer array [hours, minutes] -> "{hours}:{minutes}"
+function format_as_time(time) {
+	// Prepend 0 to minutes writable with 1 digit.
+	if (time[1].toString().length == 1) {
+		time[1] = "0" + time[1].toString();
+	}
+	return `${time[0]}:${time[1]}`;
+}
+
 function append_header() {
 	const col = document.importNode(col_template.content, true);
 	const no = header_row.children.length - 1;
@@ -32,7 +41,7 @@ function append_header() {
 		let first = parse_time(begin.value);
 		if (!first) return;
 		first = add_minutes(first, LENGTH);
-		end.value = `${first[0]}:${first[1]}`;
+		end.value = format_as_time(first);
 	});
 
 	btn.parentElement.parentElement.insertBefore(col, btn.parentElement);
@@ -45,7 +54,9 @@ btn.addEventListener("click", append_header);
 
 function save() {
 	console.log("!");
-	let data = {};
+	let data = {
+		teachers: teachers
+	};
 	data.times = Array.from(header_row.children).slice(1, -1).map(child => {
 		console.log(child);
 		return {
@@ -53,6 +64,9 @@ function save() {
 			end: parse_time(child.querySelector("#end").value),
 		};
 	});
+
+	console.log(data);
+	console.log(JSON.stringify(data));
 	return data;
 }
 
@@ -66,3 +80,62 @@ new_row_btn.addEventListener("click", () => {
 	tr.appendChild(td);
 	tbl.insertBefore(tr, new_row_row);
 });
+
+
+let teachers = [];
+
+function display_teachers(teachers) {
+	teacher_list.innerHTML = "";
+	for (const teacher of teachers) {
+		let przedmioty = "";
+		for (const subject of teacher.przedmioty) {
+			przedmioty += subject;
+			przedmioty += ", ";
+		}
+		przedmioty = przedmioty.substring(0, przedmioty.length - 2);
+		teacher_list.innerHTML += ` <span>
+			<p> 
+				<span style="display: inline-block; width: 1.3ch; height: 1.3ch; background-color: ${teacher.kolor}"></span>
+				<span> ${teacher.imie} ${teacher.nazwisko}  </span>
+				<span> (${przedmioty}) </span>
+			</p>
+		</span>`;
+	}
+}
+
+add_teacher_form.addEventListener("submit", e => {
+	e.preventDefault();
+	console.log(e);
+	let teacher_data = {
+		"imie": add_teacher_form.imie.value, 
+		"nazwisko": add_teacher_form.nazwisko.value, 
+		"oddzial": add_teacher_form.oddzial.value,
+		"kolor": add_teacher_form.kolor.value,
+		"przedmioty": add_teacher_form.przedmioty.value.split(",").map(string => string.trim())
+	};
+	teachers.push(teacher_data);
+	display_teachers(teachers);
+});
+
+function load_data(data) {
+	teachers = data.teachers;
+	display_teachers(teachers);
+	for (let i = 0; i < header_row.children.length - 2; i++) {
+		header_row.children[1].remove();
+	}
+	for (let i = 0; i < data.times.length; i++) append_header();
+	let cursor = 1;
+	for (const time of data.times) {
+		header_row.children[cursor].querySelector("#begin").value = format_as_time(time.begin);
+		header_row.children[cursor].querySelector("#end").value = format_as_time(time.end);
+		console.log(time);
+		cursor += 1;
+	}
+}
+
+load_form.addEventListener("submit", e => {
+	e.preventDefault();
+	let data = load_form.querySelector("textarea").value;
+	data = JSON.parse(data);
+	load_data(data);
+})
